03f6cef308596c1181c3f64a756f155c8274cc5d
Mbotskuy <56393107+mbotskuy@users.noreply.github.com>
2023-05-05 06:46:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,95 @@
 let previousResultContainer = null;
 
+function showLoader() {
+
+        const loader = document.getElementById("img");
+
+        loader.style.display = "block";
+
+      }
+
 function convertVideo() {
+
+const loader = document.getElementById("img");
+
+        loader.style.display = "block";
+
   const url = document.getElementById("url-input").value;
+
   fetch("https://ssyoutube.com/api/convert", {
+
     method: "POST",
+
     body: JSON.stringify({ "url": url }),
+
     headers: {
+
       "Content-Type": "application/json"
+
     }
+
   })
+
   .then(response => response.json())
+
   .then(data => {
+
     console.log(data);
 
     const resultContainer = document.createElement("div");
-    const title = data.meta.title;
-    const thumb = data.thumb;
-    const hosting = data.hosting;
-    const duration = data.meta.duration;
-    const videoUrl = data.url[0].url;
-    
-    resultContainer.innerHTML = `<div class="result-title">Video ${hosting}</div>`;
-    resultContainer.innerHTML += `<img src="${thumb}" style="width: 250px; height: auto;"/>`;
-resultContainer.innerHTML += `<div class="result-title">${title}</div>`;
-    resultContainer.innerHTML += `<div>Durasi ${duration}</div>`;
 
+    const title = data.meta.title;
+
+    const thumb = data.thumb;
+
+    const hosting = data.hosting;
+
+    const duration = data.meta.duration;
+
+    const videoUrl = data.url[0].url;
+
+    
+
+                             
+
+    resultContainer.innerHTML = `<div class="result-title">Sumber video ${hosting}</div>`;
+
+    resultContainer.innerHTML += `<img src="${thumb}" style="width: 250px; height: auto;"/>`;
+
+resultContainer.innerHTML += `<div class="result-title">${title}</div>`;
+
+    resultContainer.innerHTML += `<div class="result-title1">Duras ${duration}</div>`;
 
     const downloadButton = document.createElement("button");
+
     downloadButton.textContent ="Download";
+
     downloadButton.addEventListener("click", () => {
+
       window.open(videoUrl, "_blank");
+
     });
+
     resultContainer.appendChild(downloadButton);
+
     
 
     if (previousResultContainer !== null) {
+
       previousResultContainer.remove();
+
     }
 
     document.body.appendChild(resultContainer);
+
     previousResultContainer = resultContainer;
+
+loader.style.display = "none";
+
+          delete window.showLoader;
+
   })
+
   .catch(error => console.error(error));
+
 }
